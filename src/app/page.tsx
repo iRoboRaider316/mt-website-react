c6fc9467b97ec798d4525e7dafb96eceb08dce97
Ian Hubble <roboianhubble@gmail.com>
2024-07-02 22:50:09 -0500

    Structure for JSON file containing project descriptions established
    Also updated which projects I will include in the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import ThemeButton from "./props/theme-button"
 import ProjectButton from "./props/project-button"
 
+import desc_data from './data/descriptions.json';
+
 export default function Page() {
     return (
         <main>
@@ -14,23 +16,26 @@ export default function Page() {
             The Ruins
             Spunky Spore
             Bouncing Bob
+            Aimer Sans Voir
 
             All Prototypes
             2.5D Test
             Steve's Adventure
+            One Block Upon A Time
             Rise To Battle
+            Tetris AI
             */}
             <div id="slide-left">
                 <p id="project-description">Wow, you can slide the text!</p>
                 <div id="projects-region">
-                    <ProjectButton projectID="project1" projectName="Bouncing Escape"/>
-                    <ProjectButton projectID="project2" projectName="The Ruins"/>
-                    <ProjectButton projectID="project3" projectName="Spunky Spore"/>
+                    <ProjectButton projectID="project1" projectName={desc_data.descriptions[1].project}/>
+                    <ProjectButton projectID="project2" projectName={desc_data.descriptions[2].project}/>
+                    <ProjectButton projectID="project3" projectName={desc_data.descriptions[3].project}/>
                 </div>
                 <div id="prototypes-region">
-                    <ProjectButton projectID="project4" projectName="2.5D Prototype"/>
-                    <ProjectButton projectID="project5" projectName="Steve's Adventure"/>
-                    <ProjectButton projectID="project6" projectName="One Block Upon A Time"/>
+                    <ProjectButton projectID="project4" projectName={desc_data.descriptions[5].project}/>
+                    <ProjectButton projectID="project5" projectName={desc_data.descriptions[6].project}/>
+                    <ProjectButton projectID="project6" projectName={desc_data.descriptions[7].project}/>
                 </div>
             </div>
             <div id="footer">
